91fbc920b0d88eaf20fc3092117b24de21acd433
oeunkyoung <ou1112@naver.com>
2022-04-23 20:22:24 +0900

    feat(CarName) : 자동차 이름 검증(5글자 이내) 및 테스트 구현

---
+++ src/main/java/racingcar/constant/Message.java
@@ -0,0 +1,11 @@
+package racingcar.constant;
+
+public class Message {
+
+    /**
+     * Error Message
+     */
+    public static final String ERR_PREFIX = "[ERROR] ";
+    public static final String ERR_NAME_LEN = ERR_PREFIX + "자동차 이름은 5글자 이내여야 합니다.";
+    public static final String ERR_MOVECNT_FORMAT = ERR_PREFIX + "0 이상의 정수를 입력해주세요.";
+}
